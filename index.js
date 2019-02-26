928fc6451cfed48d77822acde866b16299ab1f42
Paul Blake <pnblake@gmail.com>
2019-02-26 13:55:32 -0500

    Begin building out index.js. Comment-out dependencies needed for authentication - will come back to that at a later date.

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const parser = require("body-parser");
 const methodOverride = require("method-override");
-const cookieParser = require("cookie-parser");
-const flash = require("connect-flash");
-const session = require("express-session");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
+// const cookieParser = require("cookie-parser");
+// const flash = require("connect-flash");
+// const session = require("express-session");
+// const passport = require("passport");
+// const LocalStrategy = require("passport-local").Strategy;
+
+app.set("view engine", "hbs");
+app.use(parser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
