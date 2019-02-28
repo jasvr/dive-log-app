6daa2000294e1a4ec10045ab141442c567422d7c
Paul Blake <pnblake@gmail.com>
2019-02-27 23:27:09 -0500

    Update for read, create and delete on the divesite model. Updates seed to begin adding dives.

--- controllers/divesite.js
+++ controllers/divesite.js
@@ -14,5 +14,21 @@ module.exports = {
   },
   newDivesite: function(req, res) {
     res.render("divesite/newDivesite");
+  },
+  createDivesite: function(req, res) {
+    const { name, coordinates, type, description } = req.body;
+    Divesite.create({
+      name,
+      coordinates,
+      type,
+      description
+    }).then(newDivesite => {
+      res.redirect(`/divesite/${newDivesite._id}`)
+    });
+  },
+  deleteDivesite: function(req, res) {
+    Divesite.deleteOne({_id: req.params.id}).then(divesite => {
+      res.redirect("/");
+    })
   }
 };
--- routes/divesite.js
+++ routes/divesite.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const divesiteController = require("../controllers/divesite");
 
 router.get("/new-dive-site", divesiteController.newDivesite);
+router.post("/new-dive-site", divesiteController.createDivesite);
 router.get("/all", divesiteController.showAllDivesites);
 router.get("/:id", divesiteController.showDivesite);
+router.delete("/:id", divesiteController.deleteDivesite);
+
 
 module.exports = router;
